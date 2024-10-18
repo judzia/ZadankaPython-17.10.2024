8f4e1df9ebd6744ee773eb2a79404cfc51aba260
Julia Pawlik <Julia.m.p@interia.pl>
2024-10-18 16:28:27 +0200

    zad1

--- ZadankaPython/ZadankaPython.py
+++ ZadankaPython/ZadankaPython.py
@@ -1,1 +1,18 @@
+# zad 1
+
+temp = float(input("Podaj temperature w Kelwinach: "))
+liczba_neutronow = float(input("Podaj liczbe neutronow: "))
+
+
+def is_criticality_balanced(temp, liczba_neutronow):
+
+    iloczyn=temp*liczba_neutronow
+
+    temperatura_ok = temp < 800
+    liczba_neutronow_ok = liczba_neutronow >500
+    iloczyn_ok=iloczyn < 500000
+
+    return temperatura_ok and liczba_neutronow_ok and iloczyn_ok #da false przy conajmniej jednym nieprawidlowym
+
+print(is_criticality_balanced(temp, liczba_neutronow))
 
